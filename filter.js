b8dd66fb6d7d9748147890543b013c0c7b8acbcc
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-07-19 23:12:58 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,55 @@
+
+let data = [
+    {
+        category: "people",
+        src: "people1.jpg"
+    },
+    {
+        category:"people",
+        src:"people2.jpg"
+
+    },
+    {
+        category:"people",
+        src:"people3.jpg"
+    },
+    {
+        category: "cars",
+        src: "cars1.jpg"
+    },
+    {
+        category:"cars",
+        src:"cars2.jpg"
+
+    },
+    {
+        category:"cars",
+        src:"cars3.jpg"
+    },
+    {
+        category: "nature",
+        src: "lights.jpg"
+    },
+    {
+        category:"nature",
+        src:"mountains.jpg"
+
+    },
+    {
+        category:"nature",
+        src:"nature.jpg"
+    }
+]
+const container= document.getElementById('container');
+function filterSelector(selectElement){
+    container.innerHTML="";
+    for(let i=0; i<data.length; i++){
+        if( data[i].category.toLowerCase()!=selectElement.toLowerCase() && selectElement!='all' ){
+            
+            continue;
+        }
+        
+        container.innerHTML+=`<img src="${data[i].src}">`
+    }
+
+}
