c9e22b14be634a8fd83f894c6c13e5309033d398
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-07-24 11:03:16 +0100

    deleted files

--- filter.js
+++ filter.js
@@ -1,4 +1,3 @@
-
 function submitForm(){
     document.getElementById('submitForm').innerHTML= 
     "Successful! Your order has been recorded and will be received within the next hour<nbr>Thanks for choosing us";
